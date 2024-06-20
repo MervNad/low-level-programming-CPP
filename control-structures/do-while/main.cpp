62e54e6e95ca1170e146162b91b92dedd68ff5b5
Merveilles <katumbm053@ecc.edu>
2024-06-20 13:38:13 -0400

    cpp do while

---
+++ control-structures/do-while/main.cpp
@@ -0,0 +1,18 @@
+// do while loop
+// writing a simple menu in C++
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	
+	cout << "----------------------------\n"
+	     << "1. Do this\n"
+	     << "2. Do that\n"
+	     << "3. Do something else\n"
+	     << "Q. Quit\n"
+	     << "Enter your selection: ";
+	cout << endl;
+	return 0;
+}
